250cbafd4aeb0115993923dfbea6ca0e4df16b88
peterseifen <peter.seifen23@sitechhs.com>
2021-05-14 10:07:58 -0400

    yes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,48 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "fe50100f2b6a46759d3568d66cfde90a";
+
+const query = async function () {
+  try {
+    const response = await fetch(`
+https://api.spoonacular.com/recipes/search?query=breakfast&sortDirection=desc&apiKey=${key}`);
+    const data = await response.json();
+    data.results.forEach((breakfast)=>{
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="movie-card">
+        <div class="movie-card-front">
+          <img
+            src="${breakfast.image}"
+            alt=""
+            class="poster"
+          />
+        </div>
+        <div class="movie-card-back">
+          <h3 class="movie-card-header">${breakfast.title}</h3>
+          <div class="score-box">
+            <p class="user-score">Timing</p>
+            <p class="user-score">${breakfast.readyInMinutes}</p>
+          </div>
+
+          <div class="release-box">
+            <p class="release-date">Servings</p>
+            <p class="release-date">${breakfast.servings}</p>
+          </div>
+
+          <div class="movie-genres">
+            <li class="movie-genre">Sci-Fi</li>
+            <li class="movie-genre">Fantasy</li>
+            <li class="movie-genre">Horror</li>
+          </div>
+        </div>
+      </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("hey something went wrong");
+  }
+};
+query();
