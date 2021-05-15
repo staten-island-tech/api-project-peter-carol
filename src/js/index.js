7e62ec98767fad7a73f7d0a30071618a501f0bca
peterseifen <peter.seifen23@sitechhs.com>
2021-05-15 16:02:16 -0400

    yes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// import { DOMSelectors } from "./DOM";
+ import { DOMSelectors } from "./DOM";
 // import { genres } from "./genre";
 
 // const key = "YOURKEYHERE";
@@ -7,30 +7,21 @@ const query = async function () {
   try {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
     const data = await response.json();
-    data.meals.forEach((breakfast)=>{
+    data.meals.forEach((meal)=>{
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
-        ` <div class="movie-card">
-        <div class="movie-card-front">
-          <img
-            src="${breakfast.strMealThumb}"
-            alt=""
-            class="poster"
-          />
-        </div>
-        <div class="movie-card-back">
-          <h3 class="movie-card-header">${breakfast.strMeal}</h3>
-          <div class="score-box">
-            <p class="user-score"></p>
-            <p class="user-score">${breakfast.strCategory}</p>
-          </div>
-
-          <div class="release-box">
-            <p class="release-date"></p>
-            <p class="release-date">${breakfast.strArea}</p>
-          </div>
-     </div>
-      </div>`
+        `<section id="recipes">
+      <div class="recipe-card">
+        <div class="recipe-card-box">
+          <div class="recipe-box-text">
+            <div class="recipe-card-text recipe-name">${meal.strMeal}</div>
+            <div class="recipe-card-text recipe-cuisine">Cuisine: ${meal.strArea} </div>
+            <div class="recipe-card-text recipe-info">Ingredients <br> This meal's main ingredients are ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}. </div>
+            <a class="flex-align-jcontent recipe-btn" href="#recipes">Read More</a>
+             </div> <!-- recipte-box-text end -->
+        </div> <!-- recipe-card-box end -->
+      </div> <!-- recipe-card end -->
+    </section> <!-- recipes end -->`
       );
     });
   } catch (error) {
