617eb1f05fe8aa338dc048477b39ffa56c088d19
peterseifen <peter.seifen23@sitechhs.com>
2021-05-15 15:14:10 -0400

    yes :)

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,32 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "fe50100f2b6a46759d3568d66cfde90a";
 
 const query = async function () {
   try {
-    const response = await fetch(`
-https://api.spoonacular.com/recipes/search?query=breakfast&sortDirection=desc&apiKey=${key}`);
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
     const data = await response.json();
-    data.results.forEach((breakfast)=>{
+    data.meals.forEach((breakfast)=>{
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         ` <div class="movie-card">
         <div class="movie-card-front">
           <img
-            src="${breakfast.image}"
+            src="${breakfast.strMealThumb}"
             alt=""
             class="poster"
           />
         </div>
         <div class="movie-card-back">
-          <h3 class="movie-card-header">${breakfast.title}</h3>
+          <h3 class="movie-card-header">${breakfast.strMeal}</h3>
           <div class="score-box">
-            <p class="user-score">Preparation Time</p>
-            <p class="user-score">${breakfast.readyInMinutes}</p>
+            <p class="user-score"></p>
+            <p class="user-score">${breakfast.strCategory}</p>
           </div>
 
           <div class="release-box">
-            <p class="release-date">Servings</p>
-            <p class="release-date">${breakfast.servings}</p>
+            <p class="release-date"></p>
+            <p class="release-date">${breakfast.strArea}</p>
           </div>
      </div>
       </div>`
@@ -36,7 +34,7 @@ https://api.spoonacular.com/recipes/search?query=breakfast&sortDirection=desc&ap
     });
   } catch (error) {
     console.log(error);
-    alert("hey something went wrong");
+    alert("Sorry! Seems like something went wrong.");
   }
 };
 query();
