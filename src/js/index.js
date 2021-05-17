fa17bed5e52507ef403c7595bb1f881d1c6664f2
peterseifen <peter.seifen23@sitechhs.com>
2021-05-17 15:04:58 -0400

    g

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,9 @@
 
 const query = async function () {
   try {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=`
+    );
     const data = await response.json();
     data.meals.forEach((meal)=>{
       let instructions = meal.strInstructions.substring(0, 229);
