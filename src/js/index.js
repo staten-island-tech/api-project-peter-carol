af5fbefd5acc6bfc4001101bfbc19573ad8a267a
peterseifen <peter.seifen23@sitechhs.com>
2021-05-17 12:16:09 -0400

    wow

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ const query = async function () {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
     const data = await response.json();
     data.meals.forEach((meal)=>{
+      let instructions = meal.strInstructions.substring(0, 249);
+      if (meal.strInstructions.length>249)
+      instructions = instructions + "...";
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         `<section id="recipes">
@@ -19,7 +22,7 @@ const query = async function () {
           
             <div class="recipe-card-text recipe-name">${meal.strMeal}</div>
             <div class="recipe-card-text recipe-cuisine">Cuisine: ${meal.strArea} </div>
-            <div class="recipe-card-text recipe-info">Ingredients <br> This meal's main ingredients are ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, and ${meal.strIngredient4}. </div>
+            <div class="recipe-card-text recipe-info">Ingredients <br> ${instructions} </div>
             <a class="flex-align-jcontent recipe-btn" href="#recipes">Read More</a>
              </div> <!-- recipte-box-text end -->
         </div> <!-- recipe-card-box end -->
