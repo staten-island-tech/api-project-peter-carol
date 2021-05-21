3aeba09edd5fc35a7d6c5b9d6431302c4d69b0a2
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-05-21 11:32:57 -0400

    .

--- src/js/index.js
+++ src/js/index.js
@@ -15,24 +15,29 @@ const query = async function () {
         instructions = instructions + "...";
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
+
         `<section id="recipes">
-      <div class="recipe-card">
-        <div class="recipe-card-box">
-        
-        <img src="${meal.strMealThumb}" alt="" class="meal-image">
-          <div class="recipe-box-text">
+        <div class="recipe-card">
+          <img src="${meal.strMealThumb}" alt="" class="meal-image">
+              <div class="recipe-card-box">
+  
+                <h1 class="recipe-nohover-name">Title</h1>
+  
+                <div class="recipe-box-text">
+  
+                  <div class="recipe-name">Title</div>
+                  <div class="recipe-cuisine"> Cuisine </div>
+                  <div class="recipe-info"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex asperiores consequuntur vero! Totam temporibus dolorem repudiandae numquam facilis ut fuga animi odit aliquam autem in dolorum, sint aperiam officiis eos? </div>
+                  <a class="flex-align-jcontent recipe-btn" href="#recipes">Read More</a>
+  
+                </div> <!-- recipte-box-text end -->
+  
+              </div> <!-- recipe-card-box end -->
           
-            <div class="recipe-card-text recipe-name">${meal.strMeal}</div>
-            <div class="recipe-card-text recipe-cuisine">Cuisine: ${meal.strArea} </div>
-            <div class="recipe-card-text ">
-            <span class="recipe-title" >Instructions</span>
-            <p class="recipe-info" >${instructions}</p> 
-            </div>
-            <a class="flex-align-jcontent recipe-btn" href="#recipes">Read More</a>
-             </div> <!-- recipte-box-text end -->
-        </div> <!-- recipe-card-box end -->
-      </div> <!-- recipe-card end -->
-    </section> <!-- recipes end -->`
+        </div> <!-- recipe-card end -->
+  
+  
+      </section> <!-- recipes end -->`
       );
     });
   } catch (error) {
